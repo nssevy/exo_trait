ab74e2822bb54cfe9c30fabaedf6fdef5b5c4778
Yves <sevyns.pro@gmail.com>
2026-08-14 20:08:58 +0700

    exo fini

--- examples/employes.rs
+++ examples/employes.rs
@@ -1,3 +1,43 @@
-fn main() {
+#![allow(unused)]
+trait Employe {
+    fn nom(&self) -> String;
+    fn salaire(&self) -> u32 {
+        2000
+    }
+}
 
+struct Stagiaire {
+    nom: String,
+}
+
+struct Manager {
+    nom: String,
+}
+
+impl Employe for Stagiaire {
+    fn nom(&self) -> String {
+        self.nom.clone()
+    }
+}
+
+impl Employe for Manager {
+    fn nom(&self) -> String {
+        self.nom.clone()
+    }
+
+    fn salaire(&self) -> u32 {
+        4500
+    }
+}
+
+fn affiche_paie<T: Employe>(employe: &T) {
+    println!("{} : {} euros", employe.nom(), employe.salaire())
+}
+
+fn main() {
+    let alice = Stagiaire {nom: String::from("Alice")};
+    let bob = Manager {nom: String::from("Bob") };
+
+    affiche_paie(&alice);
+    affiche_paie(&bob);
 }
