6ada2d679528a0d7866507028c4461409d2dc90e
Yves <sevyns.pro@gmail.com>
2026-08-14 19:53:06 +0700

    nouveau exo

---
+++ target/debug/examples/employes-382f17b89cc13157.d
@@ -0,0 +1,5 @@
+/Applications/XAMPP/xamppfiles/htdocs/exo_trait/target/debug/examples/employes-382f17b89cc13157.d: examples/employes.rs
+
+/Applications/XAMPP/xamppfiles/htdocs/exo_trait/target/debug/examples/employes-382f17b89cc13157: examples/employes.rs
+
+examples/employes.rs:
---
+++ target/debug/examples/employes.d
@@ -0,0 +1,1 @@
+/Applications/XAMPP/xamppfiles/htdocs/exo_trait/target/debug/examples/employes: /Applications/XAMPP/xamppfiles/htdocs/exo_trait/examples/employes.rs
