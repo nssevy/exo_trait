055eb2e4ecd3b597b72962fa3634a59b5015e3ae
Yves <sevyns.pro@gmail.com>
2026-08-14 19:24:04 +0700

    Exo fini, premiere test avec les Super Trait

---
+++ examples/vehicule_serie.rs
@@ -0,0 +1,35 @@
+#![allow(unused)]
+
+trait Machine {
+    fn numero_serie(&self) -> u32;
+}
+
+trait Vehicule: Machine {
+    fn modele(&self) -> String;
+    fn presentation(&self) -> String {
+        format!("{} (numero de serie : {})", self.modele(), self.numero_serie())
+    }
+}
+#[derive(Debug)]
+struct Camion {
+    modele: String,
+    numero_serie: u32,
+}
+
+impl Machine for Camion {
+    fn numero_serie(&self) -> u32 {
+        self.numero_serie
+    }
+}
+
+impl Vehicule for Camion {
+    fn modele(&self) -> String {
+        self.modele.clone()
+    }
+}
+
+fn main(){
+    let camion = Camion {modele: String::from("Volvo FH"), numero_serie: 4021 };
+    println!("{}", camion.presentation());
+    println!("{:?}", camion);
+}
---
+++ target/debug/examples/vehicule_serie.d
@@ -0,0 +1,1 @@
+/Applications/XAMPP/xamppfiles/htdocs/exo_trait/target/debug/examples/vehicule_serie: /Applications/XAMPP/xamppfiles/htdocs/exo_trait/examples/vehicule_serie.rs
