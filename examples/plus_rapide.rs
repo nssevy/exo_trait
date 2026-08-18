35aa147f6ff1c7da6e3a9f9ade667c370495a417
Yves <sevyns.pro@gmail.com>
2026-08-18 23:36:28 +0200

    Petite correction

--- examples/plus_rapide.rs
+++ examples/plus_rapide.rs
@@ -1,5 +1,4 @@
-#![allow(unused)]
-
+//#[allow(dead_code)]
 trait Vehicule {
     fn nom(&self) -> String;
     fn vitesse(&self) -> u32;
@@ -9,6 +8,12 @@ trait Vehicule {
 struct Voiture {
     nom: String,
     vitesse: u32,
+}
+
+impl Voiture {
+    fn new(n: String, v: u32) -> Voiture {
+        Voiture { nom: n, vitesse: v }
+    }
 }
 
 impl Vehicule for Voiture {
@@ -21,26 +26,20 @@ impl Vehicule for Voiture {
     }
 }
 
-impl Voiture {
-    fn new(n: String, v: u32) -> Voiture {
-        Voiture { nom: n, vitesse: v }
-    }
-}
-
 fn plus_rapide(voitures: &Vec<Voiture>) -> Option<String> {
     if voitures.is_empty() {
         return None;
     } else {
-        let mut comparant = voitures[0].vitesse;
+        let mut comparant = voitures[0].vitesse();
         let mut vr: &Voiture = &voitures[0]; //vr pour voiture rapide
 
         for voiture in voitures {
-            if voiture.vitesse > comparant {
+            if voiture.vitesse() > comparant {
                 vr = voiture;
-                comparant = voiture.vitesse;
+                comparant = voiture.vitesse();
             }
         }
-        Some(vr.nom.clone())
+        Some(vr.nom())
     }
 }
 
@@ -55,9 +54,9 @@ fn print_vehicule(v: &Option<String>) {
 
 fn main() {
     //Les voitures initialiser
-    let mut voiture_n1: Voiture = Voiture::new(String::from("Bolide"), 23_000);
-    let mut voiture_n2: Voiture = Voiture::new(String::from("Berline"), 10_000);
-    let mut voiture_n3: Voiture = Voiture::new(String::from("Citadine"), 5_000);
+    let voiture_n1: Voiture = Voiture::new(String::from("Bolide"), 23_000);
+    let voiture_n2: Voiture = Voiture::new(String::from("Berline"), 10_000);
+    let voiture_n3: Voiture = Voiture::new(String::from("Citadine"), 5_000);
 
     //Les tableaux, un remplis de voiture, un autre vide.
     let tab_de_voiture: Vec<Voiture> = vec![voiture_n1, voiture_n2, voiture_n3];
@@ -71,9 +70,3 @@ fn main() {
     print_vehicule(&pr);
     print_vehicule(&v);
 }
-
-/*
-Dans main , crée un Vec de plusieurs voitures
-appelle plus_rapide ,
-et affiche le résultat avec un match sur l’ Option ( Some(nom) -> affiche, None -> message «
-aucun véhicule »).*/
