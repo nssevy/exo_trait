17458f6a83ed49b9137f1e799c073c7cb325b895
Yves <sevyns.pro@gmail.com>
2026-08-22 16:05:13 +0200

    exo fini

--- examples/algo_k.rs
+++ examples/algo_k.rs
@@ -1,56 +1,56 @@
-#![allow(unused)]
+//#![allow(unused)]
 
 //Écris une fonction qui reçoit une slice d'entiers et renvoie un Vec<(i32, usize)> : chaque valeur suivie du nombre de fois qu'elle se répète consécutivement.
 
 fn x_fois(tab: &[i32]) -> Vec<(i32, usize)> {
     let mut i: usize = 0;
     let mut tableau = Vec::new();
-    let mut a: usize = 1;
-    let mut tuple: (i32, usize) = (0,0);
 
-    //let mut tuple: (i32, usize) = (0,0);
+    if tab.is_empty() {
+        return tableau;
+    }
+
+    let mut count: usize = 1;
+    let mut tuple: (i32, usize) = (tab[0],1);
 
     while i+1 < tab.len() {
-        let mut courant: i32 = tab[i+1];
-        let mut suivant: i32 = tab[i+1];
+        let courant: i32 = tab[i];
+        let suivant: i32 = tab[i+1];
 
         if courant == suivant {
-            a += 1;
+            count += 1;
         } else {
             tableau.push(tuple);
-            a = 0;
+            count = 1;
         }
 
-        tuple = (courant, a);
-
-        //tableau.push(tuple);
-
-
-        dbg!(&tuple);
+        tuple = (suivant, count);
 
         i+= 1;
     }
 
-    //tableau.push(tuple);
+    tableau.push(tuple);
 
     tableau
 }
 
 fn main() {
 
-    /*let mut tab = Vec::new();
-    let tuple: (i32, i32) = (1, 2);
-
-    tab.push((tuple));
-    tab.push((5, 3));
-
-    dbg!(&tab); */
-
     let tab1: Vec<i32> = vec![1, 1, 1, 2, 2, 3];
     println!("{:?} : {:?}", &tab1, x_fois(&tab1));
 
-    /*let tab5: Vec<i32> = vec![];
-    dbg!(x_fois(&tab5));*/
+    let tab2: Vec<i32> = vec![1, 2, 1];
+    println!("{:?} : {:?}", &tab2, x_fois(&tab2));
+
+    let tab3: Vec<i32> = vec![4, 4, 4, 4];
+    println!("{:?} : {:?}", &tab3, x_fois(&tab3));
+
+    let tab4: Vec<i32> = vec![7];
+    println!("{:?} : {:?}", &tab4, x_fois(&tab4));
+
+    let tab5: Vec<i32> = vec![];
+    println!("{:?} : {:?}", &tab5, x_fois(&tab5));
+
 }
 
 /*
